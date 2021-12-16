7d43aa8e1b5f1decd40373ec625fa1836b555a29
KennyCarreno25 <kcarreno179@unab.edu.co>
2021-12-15 20:56:28 -0500

    service

--- employeemanagerapp/src/app/employee.service.ts
+++ employeemanagerapp/src/app/employee.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
-@Injectable({providedIn: 'root'})
-export class ServiceNameService {
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
   private apiServerUrl = '';
   constructor(private httpClient: HttpClient) { }
   
-  public getEmployees(): Observable<any>{
-    return this.httpClient.get<any>(`${this.apiserverUrl}/emploee/all`)
+  public getEmployees(): Observable<Employee>{
+    return this.httpClient.get<any>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  public addEmploye(employee: Employee): Observable<Employee>{
+    return this.httpClient.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
+
+  public updateEmploye(employee: Employee): Observable<Employee>{
+    return this.httpClient.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  public deleteEmploye(employeeId: number): Observable<void>{
+    return this.httpClient.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
   }
 }
